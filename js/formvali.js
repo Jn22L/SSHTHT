257c134ccf8f74442ad2d31153020af8348be3c8
jn22l <peaceofjn@gmail.com>
2020-12-07 02:07:25 +0900

    form vali

--- js/formvali.js
+++ js/formvali.js
@@ -1,5 +1,5 @@
 const name = document.getElementById("name");
-const password = document.getElementById("password");
+const age = document.getElementById("age");
 const form = document.getElementById("form");
 const errorElement = document.getElementById("error");
 
@@ -11,24 +11,14 @@ form.addEventListener("submit", (e) => {
   //     messages.push("이름을 입력해 주세요");
   //   }
 
-  if (password.value.length < 6) {
-    messages.push("패스워드는 6자 이상으로 입력해 주세요");
-  }
-
-  if (password.value.length > 20) {
-    messages.push("패스워드는 20자 이하로 입력해 주세요");
-  }
-
-  if (password.value === "password") {
-    messages.push("password 는 패스워드로 불가합니다");
+  if (age.value.length < 2) {
+    messages.push("나이는 2자 이상으로 입력해 주세요.");
   }
 
   if (messages.length > 0) {
-    //e.preventDefault();
     errorElement.innerText = messages.join(", ");
   } else {
-    //e.preventDefault();
-    let form_data = { name: name.value, password: password.value };
+    let form_data = { name: name.value, age: age.value };
     alert(JSON.stringify(form_data));
   }
 });
