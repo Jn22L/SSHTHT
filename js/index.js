881baf5e59a90c6b337564eb4cd1dd64c0d7515d
jn22l <peaceofjn@gmail.com>
2020-12-07 19:53:05 +0900

    .

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 (function () {
   const navigation = document.getElementById("navigation");
-
   const routes = {
     "/home": "./pages/home.html",
     "/formvali": "./pages/formvali.html",
@@ -9,34 +8,26 @@
     "/pushstate": "./pages/pushstate.html",
   };
 
-  function goMenu(path) {
-    //alert("goMenu url:" + getContextPath() + path);
-    //$("article").load(getContextPath() + path);
-    $("article").load(path);
-    //setUnselectedAll();
-    //document.getElementById("nav__todo").setAttribute("class", "selected");
+  function goMenu(path, route) {
+    $("article").load(route);
+
+    //선택 메뉴 표시
+    setUnselectedAll();
+    document.querySelector(`a[href="${path}"]`).setAttribute("class", "selected");
   }
 
   function setUnselectedAll() {
-    let li_tags = document.querySelectorAll("li a");
+    let li_tags = document.querySelectorAll("#navigation li a");
     li_tags.forEach(function (v) {
       console.log(v);
       v.setAttribute("class", "unselected");
     });
   }
 
-  function getContextPath() {
-    var hostIndex = location.href.indexOf(location.host) + location.host.length;
-    return location.href.substring(
-      hostIndex,
-      location.href.indexOf("/", hostIndex + 1)
-    );
-  }
-
   navigation.addEventListener("click", (e) => {
     if (!e.target.matches("#navigation > li > a")) return;
     e.preventDefault();
     const path = e.target.getAttribute("href");
-    goMenu(routes[path]);
+    goMenu(path, routes[path]);
   });
 })();
