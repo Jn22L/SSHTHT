c8566a479304ac676e273705c69bb9b1feefa338
unknown <22220252@KTNG.AD>
2020-12-17 16:31:43 +0900

    .

--- js/todolist.js
+++ js/todolist.js
@@ -62,9 +62,112 @@
     }
   }
 
+  function makeRequest() {
+    httpRequest = new XMLHttpRequest();
+
+    if (!httpRequest) {
+      alert("XMLHTTP 인스턴스를 만들 수가 없어요 ㅠㅠ");
+      return false;
+    }
+    httpRequest.onreadystatechange = alertContents;
+    httpRequest.open("GET", "https://jn22l.herokuapp.com/getKeys");
+    httpRequest.send();
+  }
+
+  function alertContents() {
+    try {
+      if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        if (httpRequest.status === 200) {
+          alert(httpRequest.responseText);
+        } else {
+          alert("There was a problem with the request.");
+        }
+      }
+    } catch (e) {
+      alert("Caught Exception: " + e.description);
+    }
+  }
+
+  // get
+  // function makeRequest2() {
+  //   fetch("https://jn22l.herokuapp.com/getKeys")
+  //     .then(function (response) {
+  //       return response.json();
+  //     })
+  //     .then(function (myJson) {
+  //       console.log(JSON.stringify(myJson));
+  //     });
+  // }
+
+  // post
+  // Example POST method implementation:
+
+  function postData(url = "", data = {}) {
+    // Default options are marked with *
+    return fetch(url, {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      mode: "cors", // no-cors, cors, *same-origin
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: "same-origin", // include, *same-origin, omit
+      headers: {
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: "follow", // manual, *follow, error
+      referrer: "no-referrer", // no-referrer, *client
+      body: JSON.stringify(data), // body data type must match "Content-Type" header
+    }).then((response) => response.json()); // parses JSON response into native JavaScript objects
+  }
+
+  function makeRequest3(url, redisKey) {
+    httpRequest = new XMLHttpRequest();
+
+    if (!httpRequest) {
+      alert("XMLHTTP 인스턴스를 만들 수가 없어요 ㅠㅠ");
+      return false;
+    }
+    httpRequest.onreadystatechange = alertContents3;
+    httpRequest.open("POST", url);
+    httpRequest.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    //httpRequest.send("id=" + encodeURIComponent(redisKey));
+    httpRequest.send();
+
+    console.log(encodeURIComponent(redisKey));
+  }
+
+  function alertContents3() {
+    try {
+      if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        if (httpRequest.status === 200) {
+          //alert(httpRequest.responseText);
+          var response = JSON.parse(httpRequest.responseText);
+          alert(response.computedString);
+        } else {
+          alert("There was a problem with the request.");
+        }
+      }
+    } catch (e) {
+      alert("Caught Exception: " + e.description);
+    }
+  }
+
   document.getElementById("btnAdd").addEventListener("click", addList); // 추가
   document.getElementById("inputAdd").addEventListener("keydown", addListEnter); // 추가 - 엔터키
   document.getElementById("btnDelAll").addEventListener("click", delAllEle); // 전체삭제
   document.getElementById("btnDelLast").addEventListener("click", delLastEle); // 마지막 요소 삭제
   document.getElementById("DeleteSel").addEventListener("click", delSelected); // 선택 삭제
+  document.getElementById("ajaxButton").addEventListener("click", makeRequest); // 조회(GET:전체목록)
+  //document.getElementById("ajaxButton2").addEventListener("click", makeRequest2); // 조회(GET:fetch사용:전체목록)
+
+  document.getElementById("ajaxButton3").onclick = function () {
+    var redisKey = document.getElementById("inputAdd").value;
+    if (redisKey === "" || redisKey == null) {
+      alert("redisKey를 입력해 주세요.");
+      return;
+    }
+
+    postData("https://jsonplaceholder.typicode.com/todos", { id: 2 })
+      .then((data) => console.log(data)) // JSON-string from `response.json()` callconsole.log(JSON.stringify(data))) // JSON-string from `response.json()` call
+      .catch((error) => console.error(error));
+  };
 })();
