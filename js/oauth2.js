6ebc1c91146fc4e9e2c11581d725e52b9eedf301
jn22l <peaceofjn@gmail.com>
2020-12-22 23:19:53 +0900

    .

--- js/oauth2.js
+++ js/oauth2.js
@@ -1,7 +1,8 @@
-(function () {
-  function init() {
-    alert("OAuth2 구글 인증해 보자구~");
-  }
+(() => {
+  const app = document.querySelector("#app");
+  const init = () => {
+    app.innerHTML = "OAuth2 사용하여 구글 인증해 보자 천천히 해보자 ";
+  };
 
   init();
 })();
