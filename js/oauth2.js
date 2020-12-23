08d4a507106fe91b46dc77e4c99b120c2b7fcc04
JN22L <peaceofjn@gmail.com>
2020-12-23 16:30:48 +0900

    vscode git연동 test

--- js/oauth2.js
+++ js/oauth2.js
@@ -1,7 +1,7 @@
 (() => {
   const app = document.querySelector("#app");
   const init = () => {
-    app.innerHTML = "OAuth2 사용하여 구글 인증해 보자 천천히 해보자 ";
+    app.innerHTML = "OAuth2 사용하여 구글 인증해 보자 천천히 해보자 ~ ";
   };
 
   init();
